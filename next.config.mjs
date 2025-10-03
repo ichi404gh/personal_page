f1b3971dff838fa5d7ad028351c5a404307490ed
Denis F <ichi404@yandex.ru>
2025-10-03 12:57:34 +0200

    cache control

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=604800, stale-while-revalidate=86400, public"
+          }
+        ]
+      },
+      {
+        source: "/cv.pdf",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=604800, stale-while-revalidate=86400, public"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
